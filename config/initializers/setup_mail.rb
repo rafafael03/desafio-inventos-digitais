166a06434973ce29a3f87c5c493624e31137d8a2
Rafael Macedo <rfmacedo@shosp.com.br>
2019-01-20 23:30:57 -0200

    smtp configuration

--- config/initializers/setup_mail.rb
+++
@@ -1,9 +0,0 @@
-ActionMailer::Base.smtp_settings = {
-        :address                        => "smtp.gmail.com",
-        :port                                        => 587,
-        :domain                         => "localhost:3000",
-        :user_name                 => "pre.order.minions@gmail.com",
-        :password                 => "inventosdigitais",
-        :authenticaton => "plain",
-        :enable_starttls_auto => true
-}
